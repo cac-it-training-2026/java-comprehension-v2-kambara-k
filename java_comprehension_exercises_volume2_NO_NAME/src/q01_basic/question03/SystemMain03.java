65b4345841013c979348ea3b820b3b91a82a73a2
kambara <kambara@C010422694.zipangu.cac.co.jp>
2026-04-28 15:10:49 +0900

    question03完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -3,7 +3,13 @@ package q01_basic.question03;
 public class SystemMain03 {
 
 	public static void main(String[] args) {
-		//TODO ここから実装する
+		Member member1 = new Member();
+		Member member2 = new Member("Miura Manabu", 28, 2);
+
+		System.out.println("---SHOW DATA(NULL)---");
+		member1.showMember();
+		System.out.println("---SHOW DATA(Miura)---");
+		member2.showMember();
 
 	}
 
