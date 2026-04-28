70aeb4b22ee1300476c9b4fd9d2f95fc6d841010
kambara <kambara@C010422694.zipangu.cac.co.jp>
2026-04-28 15:13:33 +0900

    question07完了

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question07/ConsoleReader.java
@@ -0,0 +1,34 @@
+package q01_basic.question07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 標準入力を制御するクラス
+ */
+class ConsoleReader {
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	/**
+	 * 整数の標準入力。
+	 * 
+	 * @return 入力整数
+	 * @throws IOException
+	 * @throws NumberFormatException
+	 */
+	public int inputNumber() throws IOException, NumberFormatException {
+
+		System.out.print("input number>>");
+		try {
+			String inputIdStr = br.readLine();
+			return Integer.parseInt(inputIdStr);
+		} catch (NumberFormatException e) {
+			System.out.println("error!");
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+}
