8fd124707b00d2f7b46f5ffa259b89fcb3cc60c7
kambara <kambara@C010422694.zipangu.cac.co.jp>
2026-04-28 15:10:24 +0900

    question02完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
@@ -3,7 +3,18 @@ package q01_basic.question02;
 public class SystemMain02 {
 
 	public static void main(String[] args) {
-		//TODO ここから実装する
+
+		Member member = new Member();
+		member.setAge(30);
+		member.setName("Miura Manabu");
+		member.setRank(1);
+		System.out.println("---SHOW DATA---");
+		member.showMember();
+		System.out.println(">>>RANK UP>>> ");
+		System.out.println("---GET RANK--- ");
+		member.rankUp();
+		int newRank = member.getRank();
+		System.out.println("newRank" + newRank);
 
 	}
 }
